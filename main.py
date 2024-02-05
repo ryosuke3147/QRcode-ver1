a69b0f9e3531ec9ca0b861e7b1fef721d68faae5
ryosuke3147 <e59126rt@kurume.kosen-ac.jp>
2024-02-05 15:25:15 +0900

    retry

--- main.py
+++ main.py
@@ -31,21 +31,6 @@ row = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
           '91', '92', '93', '94', '95', '96', '97', '98', '99', '100']
 
 
-
-
-# name = [
-#      '   ' , '明石凛太郎', '伊藤匠真', '伊藤千晴' , '今村 泰志' , '入江 麗姫' , 
-#      '浦塚 武士' , '江頭 弦樹' , '大坪 諒一' , '梶原 徳真' , '加藤田 悠真'  , 
-#      '河津 仁晴' , '久保田 勇気' , '江田 光来' , '古賀 結莉華' , '後藤 博喜' , 
-#      '小林 士秩' , '是松 文太郎' , '坂井 瑠衣' , '貞方 海生' , '佐野 鈴莉' , 
-#      '猿渡 希流' , '白水 希龍' , '末續 藍人' , '田中 健翔' , '田中 志保里' , 
-#      '谷口 輝太朗' , '寺島 大虎' , '寺元 一耕' , '土井 秀哉' , '富田 涼介' , 
-#      '内藤 誉起' , '中村 開斗' , '西田 大和人' , '西村 雅基' , '花岡 瑞生' , 
-#      '日吉 曜大' , '平川 粹' , '藤井 颯' , '松永 光世' , '松吉 宇宙' , 
-#      '三好 俊祐' , '森山 優星' , '山下 紘輝' , '吉山 響' , '渡辺 嵩広' , 
-#      'アマラ'
-#      ]
-
 st.title('QR出席管理')
 number_list = []
 
@@ -62,8 +47,6 @@ if st.checkbox('名前を追加する'):
         if st.form_submit_button("追加"):
             name = list(filter(None,name))
             name.append(newname)
-            # x, y = pa.locateCenterOnScreen('button2.png')
-            # pa.leftClick(x, y)
             pa.press("F5")
 
 
@@ -74,7 +57,7 @@ with open("./name_data.txt",mode="w",encoding="utf-8") as f:
     f.write(namerabel)
 
 namelength = len(name)
-# print(name)
+
 
 
 
@@ -87,15 +70,6 @@ project = book.worksheets[0]
 project.cell(row = 1, column = 1, value = "出席番号")
 project.cell(row = 1, column = 2, value = "名前")
 
-
-# if st.checkbox('新しいシートを作成します'):
-#     book.create_sheet('Sheet')
-
-# i = 1
-# kadai = [" "]
-# for row in project.iter_rows(min_col = 3, max_row = 1):
-#     for col in row:
-#         kadai.append(col.value)
 
 master_kadai = []
 with open("./exam_name.txt",mode="r",encoding="utf-8") as g:
@@ -112,15 +86,6 @@ master_kadai.append(kari)
 kadai_volume = len(newkadai)
 # print(kari)
 # print(newkadai)
-
-
-
-
-
-
-
-
-
 
 
 
@@ -144,11 +109,7 @@ if st.checkbox("ノート課題を追加する"):
             permanent_exam_name.append(permanent_title)
             master_note.append(word.replace("\n", ""))
             book.create_sheet(index = 1, title = permanent_title)
-            # sheet_volume = len(book.worksheets) + 1
-            # book.create_sheet(index = sheet_volume, title = permanent_title)
-            # pa.press("F5")
-            ##課題を新規作成した時のQRコードをファイルに保存(ノート)
-            qrcnt = 1
+
             make_dir_path = "C:\STREAMLIT\streamlit-sample\image\\" + permanent_exam_name[-1]
 
             if os.path.isdir(make_dir_path):
@@ -210,43 +171,10 @@ for i in range(permanent_volume):
     note.cell(row = 2, column = 2, value = "名前")
     startwrite()
 
-
-    # sheetname = sheetname + "1"
-
-    # book.remove(sheetname)
-    # if book != "Sheet1" or sheetname:
-    #     book.remove(book)
-    # for ws in book.worksheets:
-    #     if ws.title != "Sheet1" or sheetname:
-    #         book.remove(index = permanent_cnt + 1)
-    #         book.save('sample.xlsx')
-
-    # # book.remove(book.worksheets[1])
-    # book.save('sample.xlsx')
     
 
     permanent_cnt = permanent_cnt + 1
 
-
-
-
-# for i in range(30):
-#     D = 0
-#     ws = book[D]
-#     if ws.values == None:
-#         ws.defined_names
-#     book.save('sample.xlsx')
-
-
-
-# D = 0
-# for i in range(len(ws_sheetname)):
-#     ws = book.worksheets[D]
-#     if ws.values == None:
-#         book.remove_sheet(ws_sheetname[D])
-#     # print(ws.values)
-#     D = D + 1
-    
 
 
 ws_sheetname = book.sheetnames
@@ -260,7 +188,7 @@ time = str(time_now.month) + "/" + str(time_now.day) + " " + str(time_now.hour) 
 
 
 ##ノート課題日付
-# aa
+
 
 calender_cnt = 3
 calender = book.worksheets[1]
@@ -273,17 +201,6 @@ while calender_value != None:
     calender_cnt = calender_cnt + 1
 
 calender_list = list(filter(None,calender_list))
-
-# print(calender_list)
-
-
-
-
-# aa
-
-
-
-
 
 
 ###課題を追加する
@@ -338,7 +255,6 @@ with open("./exam_name.txt",mode="w",encoding="utf-8") as g:
     g.write(examrabel)
     
 
-# allkadai = newkadai + permanent_exam_name
 
 
 student_number = 0
@@ -352,7 +268,7 @@ number = st.sidebar.selectbox(
     'あなたの出席番号を教えてください',
     list(range(0,namelength))
 )
-# purinto = st.sidebar.toggle("プリント課題を提出する")
+
 
 
 display_newkadai = newkadai.insert(0, "選択してください")
@@ -361,14 +277,6 @@ qrkadai = st.sidebar.selectbox(
     list(newkadai)
 )
 
-# del newkadai[0]
-# print(display_newkadai)
-# print(newkadai)
-# print(qrkadai)
-
-
-# if purinto:
-#     selectprinto()
 
 
 display_notekadai = permanent_exam_name.insert(0, "選択してください")
@@ -377,7 +285,7 @@ notekadai = st.sidebar.selectbox(
     list(permanent_exam_name)
 )   
 
-qrkadai2 = newkadai.index(qrkadai) - 1      #####################
+qrkadai2 = newkadai.index(qrkadai) - 1  
 notekadai2 = permanent_exam_name.index(notekadai) - 1
 
 
@@ -390,7 +298,7 @@ else:
     time_now = datetime.datetime.now()
     st.sidebar.write(time_now)
 
-exam_number = len(newkadai)                                 #########################################
+exam_number = len(newkadai)  
 
 strnumber = str(number)
 strqrkadai2 = str(qrkadai2 + 1)
@@ -405,13 +313,7 @@ qr = qrcode.make(data = QRdata , version = 1)
 img = Image.open('number.png')
 qr.save('number.png')
 
-# draw = ImageDraw.Draw(img)
-# font = ImageFont.truetype("C:\\meiryo.ttc", 30)
-# draw.text((150, 300), newkadai[qrkadai2 + 1], anchor = 'md', font = font)
-# img.save('number.png')
 
-# dd
-# print(exam_number)
 
 if st.sidebar.checkbox('QRコードを表示しますか？'):
     if number == 0:
@@ -495,110 +397,6 @@ for i in range(len(master_note)):
             
             qrcnt = qrcnt + 1                
 
-##課題を新規作成した時のQRコードをファイルに保存(課題)
-
-# qrcnt = 1
-# make_dir_path = "C:\STREAMLIT\streamlit-sample\image\\" + newkadai[-1]
-
-# if os.path.isdir(make_dir_path):
-#         print("nande")
-# else:
-#     print("kotti")
-#     os.makedirs(make_dir_path)
-#     qrcnt = 1
-#     print(kari)
-#     kari_volume = len(kari) + 1
-#     print(kari_volume)
-#     strkari_volume = str(kari_volume)
-#     print(strkari_volume)
-    
-#     for i in range(namelength):
-#         saveqr_data = str(qrcnt) + " " + str(len(newkadai)) + " " + "0"
-#         saveqr = qrcode.make(data = saveqr_data, version = 1)
-#         saveqr.save(make_dir_path + "\\" + str(qrcnt) + ".jpg", label = str(qrcnt), quality = 75, optimize = True, progressive = True)
-#         # saveqr.show()
-            
-#         qrcnt = qrcnt + 1  
-
-# ##課題を新規作成した時のQRコードをファイルに保存(ノート)
-# qrcnt = 1
-# make_dir_path = "C:\STREAMLIT\streamlit-sample\image\\" + permanent_exam_name[-1]
-
-# if os.path.isdir(make_dir_path):
-#         print("note")
-# else:
-#     os.makedirs(make_dir_path)
-#     qrcnt = 1
-#     master_note_volume = len(master_note) + 1
-#     strmaster_note_volume = str(master_note_volume)
-
-
-#     for i in range(namelength):
-#         saveqr_data = str(qrcnt) + " " + "0" + " " + strmaster_note_volume
-#         saveqr = qrcode.make(data = saveqr_data, version = 1)
-#         saveqr.save(make_dir_path + "\\" + str(qrcnt) + ".jpg", label = str(qrcnt), quality = 75, optimize = True, progressive = True)
-#         # saveqr.show()
-            
-#         qrcnt = qrcnt + 1  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# mecchaera-haiterukoko
-
-
-# if os.path.isdir(make_dir_path):
-#     print("koko")
-#     pass
-# else:
-#     os.makedirs(make_dir_path)
-
-
-
-
-
-
-# if st.checkbox("QRコードを生成する"):
-#     img = Image.open('number.png')
-#     st.image(img, caption = strnumber + '番のQRコード', use_column_width = False)
-
-
-
-
-
-
-
-
-# if __name__ == "__main__":
-#     image00 = Image.open('C:\STREAMLIT\streamlit-sample\kote00.jpg')
-#     image01 = Image.open('C:\STREAMLIT\streamlit-sample\kote01.jpg')
-#     image02 = Image.open('C:\STREAMLIT\streamlit-sample\kote02.jpg')
-#     image03 = Image.open('C:\STREAMLIT\streamlit-sample\kote03.jpg')
-#     image04 = Image.open('C:\STREAMLIT\streamlit-sample\kote04.jpg')
-
-#     images = [image00, image01, image02, image03, image04]
-
-#     fig = plt.figure()
-#     for i, im in enumerate(images):
-#         fig.add_subplot(4,5,i+1).set_title(str(i))
-#         plt.imshow(im)
-    
-#     plt.show()
-
-
 
 
 ###エクセル初期設定
@@ -612,28 +410,7 @@ for i in range(0, kadai_volume):
     Y = newkadai[i + 1]
     project.cell(row=1, column=i + 3, value=Y)
 
-# for idx in range(1, namelength):
-#     Z = name[idx]
 
-#     note.cell(row=idx+2, column=1, value=idx)
-#     note.cell(row=idx+2, column=2, value=Z)
-
-# for i in range(0, kadai_volume):
-#     F = newkadai[i]
-#     project.cell(row=1, column=i + 3, value=F)
-
-
-###QRコードの受付
-#エクセルに〇をつける
-
-
-
-
-# unsubmited_color = PatternFill(patternType='solid', fgColor='FFFF00', bgColor= 'FFFF00')
-
-
-
-# kadaicnt = 2
 newkadaicnt = 0
 kadai_submit = []
 if st.checkbox("未提出者を表示する"):
@@ -736,12 +513,9 @@ on = st.toggle("提出モード")
 if on:
     with st.form("submitform",clear_on_submit=True):
         submission2 = st.text_input("QRコードをかざしてください")
-        # pa.press("F11")
-        # pa.moveTo(1, 1)
-        # pa.move(850,360,duration=0.1)
-        # pa.click()
+
         if st.form_submit_button("提出"):
-            # x, y = pa.locateCenterOnScreen('button2.png', confidence = 0.6)
+
             pa.click(1000, 450)
             qrread = submission2.split()
             read_row = int(qrread[0]) + 1
@@ -782,19 +556,10 @@ if on:
                     calender_list = list(filter(None,calender_list))
                     calender_volume = len(calender_list)
                     calender_time = str(time_now.month) + "." + str(time_now.day)
-                    # calender_time_2 = "'" + str(time_now.month) + "." + str(time_now.day)
-                    calender_time_2 = str(time_now.month) + "." + str(time_now.day)            ### "'" 問題
+                    calender_time_2 = str(time_now.month) + "." + str(time_now.day)           
 
 
-                    # calender_time = float(calender_time)
-
-                    
-
-                    # print(calender_time)
-
-                    # calender_list_2 = [1.18, 1.19, 1.20]
-
-                    # print(calender_time)
+                  
                     print(calender_list)
 
                     if calender_time in calender_list:
@@ -824,23 +589,8 @@ if on:
                     calender_list = list(filter(None,calender_list))
                     calender_volume = len(calender_list)
                     calender_time = str(time_now.month) + "." + str(time_now.day)
-                    # calender_time_2 = "'" + str(time_now.month) + "." + str(time_now.day)
-                    calender_time_2 = str(time_now.month) + "." + str(time_now.day)            ### "'" 問題
-
-
-                    # calender_time = float(calender_time)
-
-                    
-
-                    # print(calender_time)
-
-                    # calender_list_2 = [1.18, 1.19, 1.20]
-
-                    # print(calender_time)
-                    print(calender_list)
-
-                    # index = []
-                    
+                    calender_time_2 = str(time_now.month) + "." + str(time_now.day)   
+                   
                     
 
                     if calender_time in calender_list:
@@ -866,8 +616,7 @@ if on:
             sound = random.randint(0, 1)
 
             playsound(sound_list[sound])
-            # print(read_row)
-            # print(read_column_1)
+
 
 
 
@@ -876,76 +625,12 @@ if on:
 
 ###未提出者リスト
 
-
-# n = 0
-# for i in range(len(master_note)):
-#     master_kadai.append(master_note[n])
-#     n = n + 1
 master_kadai.append(master_note)
-# print(master_kadai)
-
-# master_kadai.append(newkadai)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 private_submit = []
 cnt = 3
 newcnt = 0
-
-
-
-
-
-
-# date_cnt = 0
-# count = -1
-# # count2 = 0
-# date_list = []
-# note_submit = []
-# note_list =[]
-# for i in range(permanent_volume):
-#     print(count)
-#     permanent_worksheet = book.worksheets[count]
-#     count = count - 1
-#     date_info = permanent_worksheet.cell(row = 2, column = date_cnt + 3).value
-#     date_cnt = 0
-#     while date_info != None:
-#         date_info = permanent_worksheet.cell(row = 2, column = date_cnt + 3).value
-#         note_list.append(date_info)
-#         date_cnt = date_cnt + 1
-#         print(note_list)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
@@ -964,10 +649,10 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
 
     date_cnt = 0
     count = -1
-    # # count2 = 0
+
     date_list = []
     note_submit = []
-    # note_submit_7 = []
+
     note_list = []
     note_list_x = []
     note_list_y = []
@@ -979,7 +664,7 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
         permanent_worksheet = book.worksheets[count]
         count = count - 1
         
-        # print(count)
+
         date_cnt = 0
         date_info = permanent_worksheet.cell(row = 2, column = date_cnt + 3).value
         
@@ -1002,40 +687,31 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
         del note_list_y[-1]
 
         note_cnt = note_cnt - 1
-            # print(date_list)
+
         
 
         
         date_list = list(filter(None,date_list))
         date_volume = len(date_list)
-        
-        # print(note_list)
-        # print(date_volume)
+
         count2 = 0
-        # print(note_cnt)
+
         for i in range(note_cnt):
             
             note_submit_info = permanent_worksheet.cell(row = number + 2, column = count2 + 3).value
-            # if note_submit_info == None:
+
             note_submit.append(note_submit_info)
             count2 = count2 + 1
-            # print(note_submit)
             
         
-    
-    
 
-    # count = count + 1
             
     note_list = [a + b for (a, b) in zip(note_list_y, note_list_x)]
-    # print(note_list_x)
-    # print(note_list_y)
-    # print(note_list)
+
 
     note_submit = [i for i, x in enumerate(note_submit)if x == None]
 
 
-    # note_submit_1 = [i - 1 for i in note_submit]
     print(note_submit)
     print("kore")
 
@@ -1047,7 +723,6 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
     until_submit_volume = len(until_submit)
     for i in range(until_submit_volume):
         A = until_submit_1[newcnt]
-        # print(A)
         B = newkadai[A]
         st.sidebar.write(B)
         newcnt = newcnt + 1
@@ -1055,7 +730,6 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
 
     newcnt = 0
     note_submit_volume = len(note_submit)
-    # print(note_submit)
     
     print(note_list)
     print(note_submit_volume)
@@ -1071,8 +745,6 @@ if st.sidebar.checkbox(b + "さんの未提出課題を表示しますか？"):
     if len(until_submit) < 1 and note_submit_volume < 1:
         st.sidebar.write("すべて提出されています!")
 
-
-    # print(note_submit)   
 
 
 print("fin")
